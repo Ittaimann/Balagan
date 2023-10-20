b8df9016cc8243932c40bed9bce21eb14d1be862
Ittai Mann <ittaiam@gmail.com>
2023-10-20 22:43:00 +0100

    fixed a seg fault, improved the clang format and uh... added debug builds! lldb is quiet cool!

--- src/Containers/String.cpp
+++ src/Containers/String.cpp
@@ -6,45 +6,49 @@ namespace BAL
 {
 
 String::String()
+	: m_size(0)
+	, m_data(nullptr)
 {
 }
 
-String::String(const char *i_string)
+String::String(const char* i_string)
+	: m_size(0)
+	, m_data(nullptr)
 {
-    Append(i_string, strlen(i_string));
+	Append(i_string, strlen(i_string));
 }
 
 String::~String()
 {
-    delete m_data;
+	delete m_data;
 }
 
-void String::Append(const char *i_string, uint32 i_length)
+void String::Append(const char* i_string, uint32 i_length)
 {
-    if (i_string == nullptr || i_length == 0)
-    {
-        return;
-    }
-    Resize(i_length + m_size);
+	if (i_string == nullptr || i_length == 0)
+	{
+		return;
+	}
+	Resize(i_length + m_size);
 }
 
 void String::Resize(uint32 i_size)
 {
-    char *tempData = new char[i_size + 1];
-    if (m_data != nullptr)
-    {
-        for (uint32 i = 0; i < m_size; i++)
-        {
-            tempData[i] = m_data[i];
-        }
-        tempData[i_size + 1] = '\0';
-        delete m_data;
-    }
-    m_data = tempData;
+	char* tempData = new char[i_size + 1];
+	if (m_data != nullptr)
+	{
+		for (uint32 i = 0; i < m_size; i++)
+		{
+			tempData[i] = m_data[i];
+		}
+		tempData[i_size + 1] = '\0';
+		delete m_data;
+	}
+	m_data = tempData;
 }
-char *String::Data()
+char* String::Data()
 {
-    return m_data;
+	return m_data;
 }
 
 } // namespace BAL
