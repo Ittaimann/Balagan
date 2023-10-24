dfe455dacfc8c7e7725dd47044a67b8e1f758f75
Ittai Mann <ittaiam@gmail.com>
2023-10-24 21:44:20 +0100

    huh

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,36 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    // things to do:
-    // 1. consume an init file
-    //      a. same directory?
-    // 2. start game loop
-    //      a. game managers?
-    // 3. start render loop
-    // 4. consume scene file
-    // 5. go go go
+	// things to do:
+	// 1. consume an init file
+	//      a. same directory?
+	// 2. start game loop
+	//      a. game managers?
+	// 3. start render loop
+	// 4. consume scene file
+	// 5. go go go
 
-    BAL::String lmao("this is also a test");
-    std::string test("this is a test");
-    std::cout << test;
-    std::cout << lmao.Data();
+	BAL::String lmao("this is also a test");
+	std::string test("this is a test");
+	std::cout << test;
+	std::cout << lmao.Data();
 
-    SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow("SDL2Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	SDL_Window* window = SDL_CreateWindow("SDL2Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
 
-    SDL_Delay(3000);
+	SDL_Delay(3000);
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
-    std::cout << test;
-    return 0;
+	std::cout << test;
+	return 0;
 }
