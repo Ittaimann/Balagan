18ff298cca84308f82bdb4c96b4cd3c7b458c316
unknown <ittaiam@gmail.com>
2023-11-19 19:07:45 +0000

    Inital take on resize for the array class. I'm so rusty at containers :)

--- Src/Containers/Array.h
+++ Src/Containers/Array.h
@@ -19,8 +19,6 @@ public:
 	}
 	T At(const uint32 i_index)
 	{
-		// TODO: error checking
-		// TODO: uh is this correct?
 		return m_data[i_index];
 	}
 	T Front();
@@ -29,9 +27,16 @@ public:
 	{
 		return m_data;
 	}
-	void Resize(uint32 i_size)
+	void Resize(const uint32 i_size)
 	{
-		(void)i_size;
+		if (i_size < m_size)
+		{
+			Expand(i_size);
+		}
+		else
+		{
+			Shrink(i_size);
+		}
 	}
 
 	uint32 Size()
@@ -40,8 +45,49 @@ public:
 	}
 
 private:
+	void Expand(const uint32 i_size)
+	{
+		char* tempData = m_data;
+
+		if (m_data != nullptr)
+		{
+			for (uint32 i = 0; i < m_size; i++)
+			{
+				tempData[i] = m_data[i];
+			}
+			delete m_data;
+		}
+		else
+		{
+			m_data = Allocate(i_size);
+		}
+
+		m_data = tempData;
+		m_size = i_size;
+	}
+
+	void Shrink(const uint32 i_size)
+	{
+		char* tempData = Allocate(i_size);
+		// assert if Data is sub that size
+		for (uint32 i = 0; i < i_size; i++)
+		{
+			tempData[i] = m_data[i];
+		}
+		delete m_data;
+		m_data = tempData;
+		m_size = i_size;
+		m_capacity = i_size;
+	}
+
+	T* Allocate(const uint32 i_size)
+	{
+		return new T[i_size];
+	}
+
 	T* m_data;
 	uint32 m_size;
+	uint32 m_capacity;
 };
 
 } // namespace BAL
