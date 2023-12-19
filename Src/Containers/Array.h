f9e9653b11c8efbc210f385cbeddc4f53a0df0bd
unknown <ittaiam@gmail.com>
2023-12-19 02:13:10 +0000

    Additional Array and String functionality. Somewhat questionable. Need to start thinking about unity testing these containers so they behave reasonably... do I wanna start makinga unit test project? might be good

--- Src/Containers/Array.h
+++ Src/Containers/Array.h
@@ -5,31 +5,23 @@
 namespace BAL
 {
 
-template <typename T> class Array
+template <typename T>
+class Array
 {
+
+	static constexpr uint32 C_GROWTH_RATE = 2u;
+
 public:
 	Array()
 		: m_data(nullptr)
 		, m_size(0)
 		, m_capacity(0){};
-	virtual ~Array()
-	{
-		delete m_data;
-	};
-	T operator[](const uint32 i_index)
-	{
-		return m_data[i_index];
-	}
-	T at(const uint32 i_index)
-	{
-		return m_data[i_index];
-	}
+	virtual ~Array() { delete m_data; };
+	T operator[](const uint32 i_index) const { return m_data[i_index]; }
+	T at(const uint32 i_index) const { return m_data[i_index]; }
 	T front();
 	T back();
-	T* data()
-	{
-		return m_data;
-	}
+	T* data() { return m_data; }
 	void resize(const uint32 i_size)
 	{
 		if (i_size < m_size)
@@ -42,15 +34,24 @@ public:
 		}
 	}
 
-	uint32 size()
+	void push_back(T i_value)
 	{
-		return m_size;
+		append(i_value);
+		m_data[m_size] = i_value;
 	}
+
+	T& push_back()
+	{
+		append();
+		return m_data[m_size];
+	}
+	uint32 size() const { return m_size; }
+	uint32 capacity() const { return m_capacity; }
 
 private:
 	void expand(const uint32 i_size)
 	{
-		char* tempData = m_data;
+		T* tempData = m_data;
 
 		if (m_data != nullptr)
 		{
@@ -66,7 +67,20 @@ private:
 		}
 
 		m_data = tempData;
-		m_size = i_size;
+		m_capacity = i_size;
+	}
+
+	void append()
+	{
+		if (m_size != m_capacity)
+		{
+			m_size += 1;
+		}
+		else
+		{
+			expand(m_size * C_GROWTH_RATE);
+			m_size += 1;
+		}
 	}
 
 	void shrink(const uint32 i_size)
@@ -83,10 +97,7 @@ private:
 		m_capacity = i_size;
 	}
 
-	T* allocate(const uint32 i_size)
-	{
-		return new T[i_size];
-	}
+	T* allocate(const uint32 i_size) { return new T[i_size]; }
 
 	T* m_data;
 	uint32 m_size;
--- Src/Containers/String.h
+++ Src/Containers/String.h
@@ -23,9 +23,31 @@ public:
 	}
 
 	virtual ~String() { delete m_data; }
-	char* operator[](int i_index);
+	char operator[](int i_index) const { return m_data[i_index]; };
 	void operator+(char i_char);
 	void operator+(String i_string);
+
+	bool operator==(const String& i_otherString)
+	{
+		// strcmp might be better tbh
+		if (i_otherString.size() != m_size)
+		{
+			return false;
+		}
+		else
+		{
+			for (uint i = 0; i < m_size; i++)
+			{
+				if (i_otherString[i] != m_data[i])
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	void operator+=(char i_char) { append(&i_char, 1); }
+	void operator+=(String i_string) { append(i_string); }
 
 	void append(const char* i_cString, uint32 i_length)
 	{
@@ -33,8 +55,16 @@ public:
 		{
 			return;
 		}
+		const uint32 preResize = m_size;
 		resize(i_length + m_size);
+		for (uint32 i = 0; i < i_length; i++)
+		{
+			// TODO: unit test this code path...
+			m_data[i + preResize] = i_cString[i];
+		}
 	}
+
+	void append(const String i_string) { append(i_string.data(), i_string.size()); }
 
 	void resize(uint32 i_size)
 	{
@@ -54,6 +84,8 @@ public:
 
 	char* data() { return m_data; }
 	const char* data() const { return m_data; };
+
+	uint size() const { return m_size; };
 
 private:
 	void cStringCopy(const char* i_string) { memcpy(m_data, i_string, m_size); }
